e5210a73993df24f676e2c848f943875b6fbcf82
Vladimir Lebedev-Schmidthof <dair@rbdev.(none)>
2013-05-19 02:08:47 +0400

    Created new Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Angband::Application.config.secret_token = '286d9336618dd9aaa1efd83ae787409fca07fcf137809596654c8ea873d5292ab47b052add863fe6a79119b0aa75efa9a0f4d1f33952e143226429c1593c261a'
